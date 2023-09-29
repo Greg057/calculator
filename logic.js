6d0394f2c5dcf6f3abe9122d61f02ea7b5c93b0a
Greg057 <88341092+Greg057@users.noreply.github.com>
2023-09-29 15:18:49 +0200

    Add basic operations

---
+++ logic.js
@@ -0,0 +1,16 @@
+function add (n1, n2) {
+    return n1 + n2;
+}
+
+function substract (n1, n2) {
+    return n1 - n2;
+}
+
+function multiply (n1, n2) {
+    return n1 * n2;
+}
+
+function divide (n1, n2) {
+    if (n2 === 0) return "ERROR";
+    return n1 / n2;
+}
