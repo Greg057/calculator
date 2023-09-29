8622103978ce0db074af14d55c0c72a01dbed424
Greg057 <88341092+Greg057@users.noreply.github.com>
2023-09-29 16:54:44 +0200

    Add event listeners and make operation function work with correct display

--- logic.js
+++ logic.js
@@ -1,18 +1,18 @@
 function add (n1, n2) {
-    return n1 + n2;
+    return +n1 + +n2;
 }
 
 function substract (n1, n2) {
-    return n1 - n2;
+    return +n1 - +n2;
 }
 
 function multiply (n1, n2) {
-    return n1 * n2;
+    return +n1 * +n2;
 }
 
 function divide (n1, n2) {
-    if (n2 === 0) return "ERROR";
-    return n1 / n2;
+    if (n2 == 0) return "ERROR";
+    return +n1 / +n2;
 }
 
 function operate (n1, operator, n2) {
@@ -29,3 +29,44 @@ function operate (n1, operator, n2) {
         return divide (n1, n2);
     }
 }
+
+function clear () {
+    displayValue = "";
+    display.textContent = "";
+    result = 0;
+}
+
+const display = document.querySelector("#display");
+const digitButtons = document.querySelectorAll(".digit");
+const operationButtons = document.querySelectorAll(".operation");
+const equalButton = document.querySelector("#equal");
+const clearButton = document.querySelector("#clear");
+let displayValue = "";
+let result = 0;
+
+
+digitButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        display.textContent += button.textContent;
+        displayValue += button.textContent;
+    })
+})
+
+operationButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        display.textContent += ` ${button.textContent} `;
+        displayValue += button.textContent;
+        operator = button.textContent;
+    })
+})
+
+equalButton.addEventListener("click", () => {
+    arr = displayValue.split(/([+\-*\/])/);
+    for (let i = 1; i<arr.length; i+2) {
+        result = operate (arr[i-1], arr[i], arr[i+1]);
+        arr.splice(i-1, 3, result);
+    }
+    display.textContent = result;
+})
+
+clearButton.addEventListener("click", clear);
