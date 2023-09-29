718270e7dacfbac1cee501724c4467a56f5f70d1
Greg057 <88341092+Greg057@users.noreply.github.com>
2023-09-29 15:32:27 +0200

    Add buttons and display

--- logic.js
+++ logic.js
@@ -14,3 +14,18 @@ function divide (n1, n2) {
     if (n2 === 0) return "ERROR";
     return n1 / n2;
 }
+
+function operate (n1, operator, n2) {
+    if (operator === "+") {
+        return add (n1, n2);
+    }
+    else if (operator === "-") {
+        return substract (n1, n2);
+    }
+    if (operator === "*") {
+        return multiply (n1, n2);
+    }
+    if (operator === "/") {
+        return divide (n1, n2);
+    }
+}
